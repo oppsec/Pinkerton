af5b2f9d099e1a8713069c5bf3423b2a5836d01d
oppsec <opps3c@gmail.com>
2022-07-28 18:56:09 -0300

    fix: fixed error when not parsing arguments

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ if __name__ == "__main__":
     banner()
 
     parser = ArgumentParser(description="List of arguments that can be passed in Pinkerton")
-    parser.add_argument("-u", help="Specify the target URL", required=False)
-    #parser.add_argument("-l", help="Specify a file with a list of URLs", required=False)
+    parser.add_argument("-u", help="Specify the target URL", required=True)
     args = parser.parse_args()
 
     check_host(args)
